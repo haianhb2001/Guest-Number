bc7705191f2245a5322bc3db81bbffe84eb97d3d
haianhb2001 <haianhb2001@gmail.com>
2022-09-15 16:27:58 +0700

    Adding a Linear Gradient

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StyleSheet, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
   
 }
